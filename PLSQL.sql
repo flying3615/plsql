de9888da10e6796a055a37103c231838e65b05d9
liuyufei <gabriel.liu3615@gmail.com>
2017-06-08 14:43:59 +1200

    init it

---
+++ PLSQL.sql
@@ -0,0 +1,198 @@
+SET SERVEROUTPUT ON;
+DECLARE
+  c_course VARCHAR2(40) := 'ITB7325 Advanced Database';
+  c_code   VARCHAR2(7);
+  c_desc   VARCHAR2(30);
+  blank_space NUMBER(2);
+  num       NUMBER(2);
+BEGIN
+  blank_space := INSTR(c_course,' ');
+  num := LENGTH(c_course);
+  c_code := SUBSTR(c_course,1,(blank_space-1));
+  c_desc := SUBSTR(c_course,(blank_space+1),(num-blank_space));
+  
+  --Display the date
+  DBMS_OUTPUT.PUT_LINE('Course Code: '||c_code);
+  DBMS_OUTPUT.PUT_LINE('Course Name: '||c_desc);
+  DBMS_OUTPUT.PUT_LINE('blank_space : '||blank_space);
+
+END;
+/
+SET SERVEROUTPUT OFF;
+
+-----------------FOR LOOP------------------
+
+DROP TABLE count_table;
+
+CREATE TABLE count_table (
+  counter NUMBER
+);
+
+
+SET SERVEROUTPUT ON;
+DECLARE
+  loop_count BINARY_INTEGER := 1;
+BEGIN
+  DELETE FROM count_table;
+  LOOP
+    INSERT INTO count_table VALUES (loop_count);
+    loop_count := loop_count + 1;
+    
+    IF loop_count >=6 THEN
+      EXIT;
+    END IF;
+  END LOOP;
+  SYS.DBMS_OUTPUT.PUT_LINE('LOOP Finished');
+END;
+/
+SET SERVEROUTPUT OFF;
+
+SELECT * FROM count_table;
+
+--------------------WHILE LOOP--------------------
+
+SET SERVEROUTPUT ON;
+DECLARE
+  loop_count BINARY_INTEGER := 1;
+BEGIN
+  DELETE FROM count_table;
+  WHILE loop_count < 6 LOOP
+    INSERT INTO count_table VALUES (loop_count);
+    loop_count := loop_count + 1;
+  END LOOP;
+  SYS.DBMS_OUTPUT.PUT_LINE('LOOP Finished');
+END;
+/
+SET SERVEROUTPUT OFF;
+
+SELECT * FROM count_table;
+
+--------------------FOR ... LOOP------------------------
+
+SET SERVEROUTPUT ON;
+DECLARE
+  loop_count BINARY_INTEGER := 1;
+BEGIN
+  DELETE FROM count_table;
+  FOR loop_count IN 1..5 LOOP
+    INSERT INTO count_table VALUES (loop_count);
+    loop_count := loop_count + 1;
+  END LOOP;
+  SYS.DBMS_OUTPUT.PUT_LINE('LOOP Finished');
+END;
+/
+SET SERVEROUTPUT OFF;
+
+SELECT * FROM count_table;
+
+-----------------Implicit Cursor---------------------
+SET SERVEROUTPUT ON;
+DECLARE
+  username member.musername%TYPE;
+  password member.mpassword%TYPE;
+BEGIN
+  SELECT musername,mpassword 
+  INTO username,password 
+  FROM member
+  WHERE ID = 1;
+  SYS.DBMS_OUTPUT.PUT_LINE('Username: '||username||' password: '||password);
+END;
+/
+SET SERVEROUTPUT OFF;
+
+---------------Explicit Cursor------------------
+SET SERVEROUTPUT ON;
+DECLARE
+  --1.Declare the cursor with query
+  CURSOR member_cursor IS
+    SELECT musername,memail,mpassword FROM member;
+  username   member.musername%TYPE;
+  email      member.memail%TYPE;
+  password   member.mpassword%TYPE;
+BEGIN
+  --2.open the cursor
+  OPEN member_cursor;
+  --3.Fetch the data rows
+  LOOP
+    FETCH member_cursor into username,email,password;
+    EXIT WHEN member_cursor%NOTFOUND; --no more rows
+    SYS.DBMS_OUTPUT.PUT_LINE('Username: '||username||' password: '||password||' email:'||email);
+  END LOOP;
+  CLOSE member_cursor;
+END;
+/
+SET SERVEROUTPUT OFF;
+
+------------Explicit Cursor with RowType----------
+SET SERVEROUTPUT ON;
+DECLARE
+  --1.Declare the cursor with query
+  CURSOR member_cursor IS
+    SELECT * FROM member;
+  member_row   member_cursor%ROWTYPE;
+BEGIN
+  --
+  .open the cursor
+  OPEN member_cursor;
+  --3.Fetch the data rows
+  LOOP
+    FETCH member_cursor INTO member_row;
+    EXIT WHEN member_cursor%NOTFOUND; --no more rows
+    SYS.DBMS_OUTPUT.PUT_LINE('Username: '||member_row.musername||' password: '||member_row.mpassword||' email:'||member_row.memail);
+  END LOOP;
+  CLOSE member_cursor;
+END;
+/
+SET SERVEROUTPUT OFF;
+
+--------------------Explicit Cursor For Loop------------------------
+
+SET SERVEROUTPUT ON;
+DECLARE
+  --1.Declare the cursor with query
+  CURSOR member_cursor IS
+    SELECT * FROM member;
+  member_row   member_cursor%ROWTYPE;
+BEGIN
+  --2.For loop no need open
+  FOR member_row IN member_cursor LOOP
+    SYS.DBMS_OUTPUT.PUT_LINE('Username: '||member_row.musername||' password: '||member_row.mpassword||' email:'||member_row.memail);
+  END LOOP;
+END;
+/
+SET SERVEROUTPUT OFF;
+
+--------------------Exception-----------------------
+
+SET SERVEROUTPUT ON;
+DECLARE
+  m_id NUMBER := &m_id;
+  CURSOR member_cursor IS
+    SELECT * FROM member WHERE id>m_id;
+  member_row   member_cursor%ROWTYPE;
+  e_foreign_key_error EXCEPTION;
+  PRAGMA EXCEPTION_INIT (e_foreign_key_error, -2291);
+  e_my_exception EXCEPTION;
+  e_id   member.id%TYPE;
+BEGIN
+  --2.For loop no need open
+  FOR member_row IN member_cursor LOOP
+    IF member_row.mmobile IS NULL THEN
+      e_id := member_row.id;
+      RAISE e_my_exception;
+    END IF;
+    SYS.DBMS_OUTPUT.PUT_LINE('Username: '||member_row.musername||' password: '||member_row.mpassword||' email:'||member_row.memail);
+  END LOOP;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    SYS.DBMS_OUTPUT.PUT_LINE('No data was found');
+  WHEN e_foreign_key_error THEN
+    SYS.DBMS_OUTPUT.PUT_LINE('Field Not found in parent table');
+  WHEN e_my_exception THEN
+    SYS.DBMS_OUTPUT.PUT_LINE('Member '||e_id||' is missing their last name');
+  WHEN OTHERS THEN
+    SYS.DBMS_OUTPUT.PUT_LINE('Encountered the following error');
+    SYS.DBMS_OUTPUT.PUT_LINE(SQLERRM);
+END;
+/
+SET SERVEROUTPUT OFF;
---
+++ StoryBoardPLSQL.sql
@@ -0,0 +1,174 @@
+SET SERVEROUTPUT ON;
+CREATE OR REPLACE PACKAGE emp_admin
+IS
+    PROCEDURE login（
+        usernameIn IN member.musername%TYPE,
+        passwordIn IN member.mpassword%TYPE
+        );
+
+    FUNCTION incomeAt(
+            year IN NUMBER,
+            month IN NUMBER
+        ) RETURN NUMBER;
+
+    PROCEDURE buy_membership（
+        usernameIn IN member.musername%TYPE,
+        amount IN NUMBER(5,2),
+        pay_type IN VARCHAR2
+        );
+
+END;
+/
+
+
+create or replace PACKAGE BODY emp_admin
+IS
+
+    ---LOGIN FUNCTION
+    PROCEDURE login(
+        usernameIn IN member.musername%TYPE,
+        passwordIn IN member.mpassword%TYPE
+        )
+    IS
+        userID member.ID%TYPE;
+        username member.musername%TYPE;
+        password member.mpassword%TYPE;
+    BEGIN
+        SELECT id,musername,mpassword 
+        INTO userID,username,password 
+        FROM member
+        WHERE musername = usernameIn;
+    
+        IF passwordIn = password THEN
+        SYS.DBMS_OUTPUT.PUT_LINE('Login Successfully');                
+        ELSE
+            SYS.DBMS_OUTPUT.PUT_LINE('Username: '||username||' Password mismatch');            
+        END IF;
+        
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        SYS.DBMS_OUTPUT.PUT_LINE('Username: '||usernameIn||' Cannot be fount');
+    END login;
+
+
+    -----INCOME IN THE GIVEN MONTH
+    FUNCTION incomeAt(
+            year_in IN NUMBER,
+            month_in IN NUMBER) RETURN NUMBER
+    IS
+        income NUMBER;
+        year_tmp NUMBER;
+        month_tmp NUMBER;
+        e_no_income_exception EXCEPTION;
+    BEGIN
+         --get current year by default
+        year_tmp := EXTRACT(year FROM sysdate);
+        IF year_in != NULL THEN
+            year_tmp := year_in;
+        END IF;
+        -- get current month by default
+        month_tmp := EXTRACT(month FROM sysdate);
+        IF month_in != null THEN
+            month_tmp := month_in;
+        END IF;
+        SYS.DBMS_OUTPUT.PUT_LINE('Find incomes in '||month_tmp||'/'||year_tmp);
+        select sum(p.pamount) into income from payment p where extract(YEAR from p.pdate) = year_tmp AND extract(MONTH from p.pdate) = month_tmp;
+        IF income = null THEN
+            RAISE e_my_exception;
+        END IF;
+        RETURN income;
+        EXCEPTION
+    WHEN e_no_income_exception THEN
+        SYS.DBMS_OUTPUT.PUT_LINE('Cannot incomes in '||month_tmp||'/'||year_tmp);
+    WHEN OTHERS THEN
+        SYS.DBMS_OUTPUT.PUT_LINE('Encountered the following error');
+        SYS.DBMS_OUTPUT.PUT_LINE(SQLERRM);
+    END incomeAt;
+
+    ---payment membership renew
+    PROCEDURE buy_membership(usernameIn IN member.musername%TYPE, weeks IN NUMBER, pay_type IN VARCHAR2,is_student BOOLEAN)
+    IS
+    member_id_tmp NUMBER;
+    days_between NUMBER;
+    att_times NUMBER;
+    attendence_rate NUMBER;
+    discount NUMBER := 1;
+    activatedate DATE;
+    fee NUMBER;
+    CURSOR c_id IS
+        SELECT id
+        FROM member
+        WHERE musername=usernameIn;
+
+    CURSOR c_activate IS
+        SELECT msactivatedate 
+        FROM member_ship ms,member m
+        WHERE ms.member_id=m.id and m.musername=usernameIn;
+    BEGIN
+        OPEN c_id;
+        FETCH c_id INTO member_id_tmp;
+        IF c_id%NOTFOUND THEN
+            DBMS_OUTPUT.PUT_LINE('User with name '||usernameIn||' not found!');
+        ELSIF c_id%FOUND THEN
+                --check if its a new user
+                OPEN c_activate;
+                FETCH c_activate INTO activatedate;
+                IF c_activate%NOTFOUND THEN
+                     DBMS_OUTPUT.PUT_LINE('New USER, No discount');
+                ELSE
+                    --check if there is an exsiting membership which hasn't been activated
+                    IF activatedate IS NULL THEN
+                        DBMS_OUTPUT.PUT_LINE('OLD USER,but there is an exsiting membership which hasnt been activated for user '||usernameIn);
+                        RETURN;
+                    END IF;
+                    DBMS_OUTPUT.PUT_LINE('OLD USER');     
+                    --get overall attendence
+                    select count(*) into att_times
+                    from attendance att,member_ship ms 
+                    where att.atend_time > ms.MSDATE and ms.member_id=att.member_id and ms.member_id=member_id_tmp group by att.member_id;
+                    --get the all days since start membership
+                    select sum(msduration)*7 into days_between from member_ship where member_id=member_id_tmp;
+                    --check the attendence
+                    attendence_rate := 100*att_times/days_between;
+                    IF attendence_rate < 50 THEN
+                        DBMS_OUTPUT.PUT_LINE('less than 50, no discount...');
+                        discount := 1;
+                    ELSIF attendence_rate = 50 and attendence_rate < 60 THEN
+                        DBMS_OUTPUT.PUT_LINE('less than 60, 10% discount...');
+                        discount := 0.9;                    
+                    ELSIF attendence_rate = 60 and attendence_rate < 70 THEN
+                        DBMS_OUTPUT.PUT_LINE('less than 70, 20% discount...');
+                        discount := 0.8;                    
+                    ELSIF attendence_rate = 70 and attendence_rate < 80 THEN
+                        DBMS_OUTPUT.PUT_LINE('less than 80, 30% discount...');
+                        discount := 0.7;                    
+                    ELSIF attendence_rate = 80 and attendence_rate < 90 THEN
+                        DBMS_OUTPUT.PUT_LINE('less than 90, 40% discount...');
+                        discount := 0.6;                    
+                    ELSIF attendence_rate >= 90 THEN
+                        DBMS_OUTPUT.PUT_LINE('greater than 90, 50% discount...');
+                        discount := 0.5;                    
+                    END IF;               
+                END IF;
+
+               
+                DBMS_OUTPUT.PUT_LINE('calculate pay amount');
+                
+                --student base is $15, adult is $20
+                IF is_student THEN
+                    fee := 15*discount;
+                ELSE 
+                    fee := 20*discount;
+                END IF;
+                
+                --insert payment table
+
+        END IF;
+        -- after payment, freeze the membership until first attendent record inserted
+    END buy_membership;
+END;
+
+--- A trigger for first attendent 
+
+
+SET SERVEROUTPUT OFF;
