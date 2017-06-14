6e39617bf13d24db405db80c3d287f04b70da448
liuyufei <gabriel.liu3615@gmail.com>
2017-06-14 13:31:25 +1200

    demo

--- uniRec/demo.sql
+++ uniRec/demo.sql
@@ -1,9 +1,11 @@
-SET SERVEROUTPUT ON;
 --John login
 exec emp_admin.login('John','123456');
 --John buys two weeks membership
 exec emp_admin.buy_membership('John',2,'VISA',true);
-
+---test trigger
+insert into attendance(atstart_time,atend_time,member_id) values (sysdate,null,1);
+--test function incomeAt
+SET SERVEROUTPUT ON;
 DECLARE
   amount       NUMBER;
   BEGIN
@@ -11,6 +13,4 @@ DECLARE
   --Display the date
   DBMS_OUTPUT.PUT_LINE('Return from function incomeAt: $'||amount);
   END;
-
----test trigger
-insert into attendance(id,atstart_time,atend_time,member_id) values (sysdate,null,1);
+/
